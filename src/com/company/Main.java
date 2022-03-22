9754a165c15d79e99f45edc1da23de9acc33d659
Evita Viska <viskaevita@gmail.com>
2022-03-22 23:12:12 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	// Program that prints out 25% of whatever value is given
+        double Percentage = 0.25;
+        int WhateverValue = 100; // Variable value
+
+        System.out.println("Percentage * WhateverValue = " + (Percentage * WhateverValue));
+
+    }
+}
